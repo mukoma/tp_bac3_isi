dedbd55cc93dc213d720010be58a4f7a8c470a98
ngonga3 <gadlumbwe8@gmail.com>
2022-07-05 11:15:07 +0200

    Creation des methodes d'acces et de modification

--- src/main/java/com/poortoys/FacesApplicationConfig.java
+++ src/main/java/com/poortoys/FacesApplicationConfig.java
@@ -11,9 +11,48 @@ import javax.faces.annotation.FacesConfig;
 @FacesConfig
 @ApplicationScoped
 public class FacesApplicationConfig {
+    private int Id;
+    private String nom;
+    private String prenom;
+    private String competence;
 
+    // le constructeur par defaut
     public FaccesApplicationConfig(){
-
+     id  = 0;
+     nom = "aucun_nom";
+     prenom = "aucun_prenom";
+     competence = "aucune competance"
     }
 
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getCompetence() {
+        return competence;
+    }
+
+    public void setCompetence(String competence) {
+        this.competence = competence;
+    }
 }
